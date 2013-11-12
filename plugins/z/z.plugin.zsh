986330a5c1388988e821487b4e2f96077ef81c03
zendy <zendywongso@gmail.com>
2013-11-12 16:15:49 +1100

    Update z

--- plugins/z/z.plugin.zsh
+++ plugins/z/z.plugin.zsh
@@ -1,1 +1,6 @@
-. `brew --prefix`/etc/profile.d/z.sh
+_load_z() {
+  source $1/z.sh
+}
+
+[[ -f $ZSH_CUSTOM/plugins/z/z.plugin.zsh ]] && _load_z $ZSH_CUSTOM/plugins/z
+[[ -f $ZSH/plugins/z/z.plugin.zsh ]] && _load_z $ZSH/plugins/z
